3eba76757d05d077c45a94c0f848529e84c79661
ayman-elkassas <aymanelkassas88@gmail.com>
2021-04-04 16:52:16 -0700

    done add first app

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [name,setName]=useState("Ahmed")
+  const [person,setPerson]=useState({name:'mohamed',age:40})
+
+  const clickHandler=()=>{
+    setName("Ayman")
+    setPerson({name:'ali',age:25})
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello ReactNative App</Text>
+        <Text style={styles.boldText}>My name is {person.name}, and my age is {person.age}</Text>
+        <Text></Text>
+        <Text style={styles.boldText}>My name is {name}</Text>
+        <View style={styles.buttonContainer}>
+          <Button title="update state" onPress={clickHandler}></Button>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header:{
+    backgroundColor:'pink',
+    padding:20
+  },
+  boldText:{
+    fontWeight:'bold'
+  },
+  buttonContainer:{
+    marginTop:30
+  }
 });
